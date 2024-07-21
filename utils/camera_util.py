17ade6cb0b1d8f0ded9562df09fa5a08183131b6
root <root@ts-751d40e37128440980798518897eda12-launcher.ts-751d40e37128440980798518897eda12-launcher.default-taiji-hyaide-hunyuan3d-nj-l40-nanjing-8.svc.cluster.local>
2024-07-22 01:38:04 +0800

    add camera_util

---
+++ utils/camera_util.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+
+
+def get_rays(pose, h, w, focal, opengl=True):
+    
+    x, y = torch.meshgrid(
+        torch.arange(w, device=pose.device),
+        torch.arange(h, device=pose.device),
+        indexing="xy",
+    )
+    x = x.flatten()
+    y = y.flatten()
+
+    cx = w * 0.5
+    cy = h * 0.5
+
+    camera_dirs = F.pad(
+        torch.stack(
+            [
+                (x - cx + 0.5) / focal,
+                (y - cy + 0.5) / focal * (-1.0 if opengl else 1.0),
+            ],
+            dim=-1,
+        ),
+        (0, 1),
+        value=(-1.0 if opengl else 1.0),
+    )  # [hw, 3]
+
+    rays_d = camera_dirs @ pose[:3, :3].transpose(0, 1)  # [hw, 3]
+    rays_o = pose[:3, 3].unsqueeze(0).expand_as(rays_d) # [hw, 3]
+
+    return rays_o, rays_d
